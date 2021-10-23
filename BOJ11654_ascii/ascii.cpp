0e3eb307248646a958d1d9beb460f68758c24d32
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2021-10-23 13:45:05 +0900

    BOJ11654_Solved1

---
+++ BOJ11654_ascii/ascii.cpp
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/11654
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char c;
+    cin >> c;
+
+    cout << (int)c << '\n';
+}
