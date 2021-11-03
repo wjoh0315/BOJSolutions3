1c807931047d47f30b99fe16a0aa3e722b38bb46
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2021-11-03 17:56:53 +0900

    BOJ1920_Solved1

---
+++ BOJ1920_findnum/findnum.cpp
@@ -0,0 +1,46 @@
+//https://www.acmicpc.net/problem/1920
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool binarysearch(int cur, int *sortedarr,
+int begin, int end)
+{
+    if (begin > end)
+        return false;
+
+    int mid = begin + (end - begin)/2;
+    if (cur < sortedarr[mid])
+        return binarysearch(cur, sortedarr, 
+            begin, mid - 1);
+    else if (cur > sortedarr[mid])
+        return binarysearch(cur, sortedarr, 
+            mid + 1, end);
+    else // cur == sortedarr[begin + (end - begin)/2]
+        return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+    for (int i=0; i < n; i++)
+        cin >> arr[i];
+    
+    sort(arr, arr+n);
+
+    int m;
+    cin >> m;
+    
+    int cur;
+    for (int i=0; i < n; i++)
+    {
+        cin >> cur;
+        cout << binarysearch(cur, arr, 0, n-1) << '\n';
+    }
+}
